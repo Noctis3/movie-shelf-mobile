fc07c0b6220388e8b0c08df169d55092768b3c1a
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-06-29 21:55:47 -0300

    feat(Home): add simple request to test axios config

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 import { IPageProps } from '../../types/navigation';
+import api from '../../services/api';
 
 export default function Home({ navigation }: IPageProps) {
+  useEffect(() => {
+    api
+      .post('authentication/token/validate_with_login', {
+        username: 'johnny_appleseed',
+        password: 'test123',
+        request_token: 'loremipsum',
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
