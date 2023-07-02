7d6eaa454e58f5d2489d6dbce5725e2ee0b72264
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-07-01 21:23:55 -0300

    feat(SignIn): add form error treatment

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, HelperText } from 'react-native-paper';
 
 import { useForm, Controller } from 'react-hook-form';
 import api from '../../services/api';
@@ -21,37 +21,39 @@ const ERROR_MESSAGES = {
 };
 
 export default function SignIn() {
-  const { control, formState, handleSubmit } = useForm<FormData>({
+  const { control, formState, handleSubmit, clearErrors, setError } = useForm<FormData>({
     mode: 'onChange',
   });
 
-  const submit = async (data: { email: string; password: string }) => {
+  const submit = async (data: { username: string; password: string }) => {
     try {
+      clearErrors();
       const response = await api.get(CREATE_REQUEST_TOKEN);
-      console.log('\nRequest token created');
+      console.log('Request token created');
       console.log(response.data);
 
       const validateResponse = await api.post(VALIDATE_REQUEST_TOKEN, {
         request_token: response.data.request_token,
-        username: data.email,
+        username: data.username,
         password: data.password,
       });
-      console.log('\nRequest token validated');
+      console.log('Request token validated');
       console.log(validateResponse.data);
 
       const sessionResponse = await api.post(CREATE_SESSION, {
         request_token: validateResponse.data.request_token,
       });
-      console.log('\nSession created');
+      console.log('Session created');
       console.log(sessionResponse.data);
 
       const accountResponse = await api.get(
         `${GET_ACCOUNT_DETAILS}?session_id=${sessionResponse.data.session_id}`
       );
-      console.log('\nAccount details');
+      console.log('Account details');
       console.log(accountResponse.data);
     } catch (error) {
       console.log(error.response.data);
+      setError('root', { type: 'manual', message: 'Erro ao autenticar' });
     }
   };
 
@@ -64,16 +66,21 @@ export default function SignIn() {
         rules={{
           required: { message: ERROR_MESSAGES.REQUIRED, value: true },
         }}
-        render={({ field }) => (
-          <TextInput
-            value={field.value}
-            label="Nome de usuário"
-            style={styles.input}
-            onBlur={field.onBlur}
-            textContentType="emailAddress"
-            autoCapitalize="none"
-            onChangeText={field.onChange}
-          />
+        render={({ field, fieldState }) => (
+          <>
+            <TextInput
+              value={field.value}
+              label="Nome de usuário"
+              style={styles.input}
+              onBlur={field.onBlur}
+              textContentType="emailAddress"
+              autoCapitalize="none"
+              onChangeText={field.onChange}
+            />
+            {fieldState.error && (
+              <HelperText type="error">{fieldState.error.message}</HelperText>
+            )}
+          </>
         )}
       />
       <Controller
@@ -83,31 +90,39 @@ export default function SignIn() {
         rules={{
           required: { message: ERROR_MESSAGES.REQUIRED, value: true },
         }}
-        render={({ field }) => (
-          <TextInput
-            value={field.value}
-            label="Senha"
-            style={styles.input}
-            onBlur={field.onBlur}
-            secureTextEntry
-            textContentType="password"
-            onChangeText={field.onChange}
-          />
+        render={({ field, fieldState }) => (
+          <>
+            <TextInput
+              value={field.value}
+              label="Senha"
+              style={styles.input}
+              onBlur={field.onBlur}
+              secureTextEntry
+              textContentType="password"
+              onChangeText={field.onChange}
+            />
+            {fieldState.error && (
+              <HelperText type="error">{fieldState.error.message}</HelperText>
+            )}
+          </>
         )}
       />
-      <Button
-        disabled={!formState.isValid}
-        mode="contained"
-        onPress={handleSubmit(submit)}
-        style={styles.button}
-      >
-        Entrar
-      </Button>
-
-      <View></View>
+      <View>
+        <Button
+          mode="contained"
+          onPress={handleSubmit(submit)}
+          style={styles.button}
+          loading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
+        {formState.errors.root && (
+          <HelperText type="error">{formState.errors.root.message}</HelperText>
+        )}
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', marginHorizontal: 30 },
